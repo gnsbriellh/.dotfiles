4a5d039d20405de9bf9c48a696af8868c075b8ad
gnsbriellh <106001507+gnsbriellh@users.noreply.github.com>
2022-07-20 22:41:17 -0300

    Fix for VScode

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-
+    source /etc/X11/xinit/xinitrc.d/50-systemd-user.sh                                  &   # see https://unix.stackexchange.com/a/295652/332452
     eval $(/usr/bin/gnome-keyring-daemon --start --components=gpg,pkcs11,secrets,ssh)   &   # Init keyring
     export GNOME_KEYRING_CONTROL GNOME_KEYRING_PID GPG_AGENT_INFO SSH_AUTH_SOCK         &   # Export keyring
+    mkdir -p "$HOME"/.local/share/keyrings                                              &   # see https://wiki.archlinux.org/title/GNOME/Keyring#xinitrc
     xrandr --output DisplayPort-2 --mode 1920x1080 --rate 165                           &   # Set monitor resolution and refresh rate
     xrandr --output LVDS-1 --mode 1366x768 --rate 60                                    &   # Set monitor resolution and refresh rate
     xset s off -dpms                                                                    &   # Disable screen saving
